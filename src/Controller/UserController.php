b7ade0859619f0c6602e20de88e7e0be0ffa6f88
Jorge Cervantes <jorgecdev@gmail.com>
2020-06-04 00:47:15 -0500

    ruta de registro de usuario, creacion del formulario de registro e impresion en la view

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,16 +4,21 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+use App\Form\RegisterType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class UserController extends AbstractController
 {
-    /**
-     * @Route("/user", name="user")
-     */
-    public function index()
+    public function userRegister(Request $request)
     {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+        $user = new User();
+        $form = $this->createForm(RegisterType::class, $user);
+
+        return $this->render('user/register.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
