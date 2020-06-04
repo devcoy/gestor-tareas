a029e9ea5666d64a361763e564b8eac58c1e2610
Jorge Cervantes <jorgecdev@gmail.com>
2020-06-03 23:22:40 -0500

    se agregaron datos dummy al archivo db.sql para insertar registros dummy a la bd

--- bd.sql
+++ bd.sql
@@ -14,6 +14,12 @@ CREATE TABLE IF NOT EXISTS Users(
   CONSTRAINT pk_users PRIMARY KEY (id)
 )ENGINE=InnoDb;
 
+INSERT INTO Users VALUES(null, 'USER_ROLE', 'Jorge', 'Cervantes', 'jorge@email.com', 'jorge123', CURTIME());
+INSERT INTO Users VALUES(null, 'USER_ROLE', 'Andrés', 'Iniestra', 'andres@email.com', 'andres123', CURTIME());
+
+
+
+
 CREATE TABLE IF NOT EXISTS Tasks(
   id int(255) auto_increment NOT NULL,
   user_id int(255) not null,
@@ -26,3 +32,8 @@ CREATE TABLE IF NOT EXISTS Tasks(
   CONSTRAINT pk_tasks PRIMARY KEY (id),
   CONSTRAINT fk_users FOREIGN KEY (user_id) REFERENCES Users(id)
 )ENGINE=InnoDb;
+
+INSERT INTO Tasks VALUES(null, 1, 'Tarea 1', 'Contenido de tarea 1', 'hight', 40, CURTIME());
+INSERT INTO Tasks VALUES(null, 2, 'Tarea 2', 'Contenido de tarea 2', 'hight', 20, CURTIME());
+INSERT INTO Tasks VALUES(null, 1, 'Tarea 3', 'Contenido de tarea 3', 'normal', 10, CURTIME());
+INSERT INTO Tasks VALUES(null, 2, 'Tarea 4', 'Contenido de tarea 4', 'hight', 24, CURTIME());
